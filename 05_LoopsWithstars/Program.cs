1108fa8bf738ae7ca8afb09deb47e31400bad766
sahinerol3534 <140306441+sahinerol3534@users.noreply.github.com>
2024-10-10 15:10:38 -0400

    Update Program.cs

--- 05_LoopsWithstars/Program.cs
+++ 05_LoopsWithstars/Program.cs
@@ -50,6 +50,101 @@ namespace _05_LoopsWithstars
 
             }
             #endregion
+
+                            #region Baklava dilimi oluşturma
+
+            int n = 5;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("             ");
+                for (int j = n - 1; j >= i; j--)
+                {
+                    Console.Write(' ');
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine(" ");
+
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("              ");
+                for (int j = 0; j <= i - 2; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (int k = 2 * n - i; k >= i + 2; k--)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine(" ");
+
+            }
+            #endregion
+
+            #region içi boş baklava dilimi
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                  *");
+            for (int i = 1; i <= n-2; i++)
+            {
+                Console.Write("             ");
+                for (int j = n-i-1; j >= 0 ; j--)
+                {
+                    Console.Write(" ");
+                }
+                  
+                Console.Write("*");
+               
+                for (int k = -1; k < (2 * i - 2 ); k++)
+                {
+                    Console.Write(" ");
+                }
+                
+                    Console.Write("*");
+              
+                 
+                
+                Console.WriteLine(" ");
+
+            }
+            //---------------------------------
+            for (int i = 1; i <= n-2; i++)
+            {
+                Console.Write("              ");
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    Console.Write(' ');
+                }
+                Console.Write('*');
+                for (int k = n+1  ; k >=  2*i-1; k--)
+                {
+                    Console.Write(' ');
+                }
+                Console.Write('*');
+
+                
+                Console.WriteLine();
+                //......................
+
+            }
+            Console.WriteLine("                  *");
+            #endregion
+
             Console.Read();
 
         }
